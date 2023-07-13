c40e2258b559b6bf2e8ee3e734edb1c71f310e75
divyanshursahu <96013623+divyanshursahu@users.noreply.github.com>
2023-07-13 19:18:46 +0530

    Basic codes

---
+++ Add Two Numbers With User Input.py
@@ -0,0 +1,6 @@
+user1 = input("Enter value 1:")
+user2 = input("Enter value 2:")
+
+sum = float(user1) + float(user2)
+print("sum of user inputs: {0}".format(sum))
+print("sum of user inputs: {0}".format(int(sum)))
---
+++ basics/hello-world.py
@@ -0,0 +1,5 @@
+# This program prints Hello, world!
+
+print("Hello","World!")
+
+'''In this program, we have used the built-in print() function to print the string Hello, world! on our screen. '''
